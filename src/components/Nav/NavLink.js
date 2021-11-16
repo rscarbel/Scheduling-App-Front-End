8bc5f47a715d97d36ccde3d843d53ffff4f0e3d4
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-16 15:39:54 -0500

    Added image prop

--- src/components/Nav/NavLink.js
+++ src/components/Nav/NavLink.js
@@ -6,10 +6,13 @@ import { Link } from 'react-router-dom';
  * @param  {String} linkLocation The react-router-dom {Link} location
  * @return {JSX}     JSX for the link
  */
-const NavLink = ({displayText, linkLocation}) => {
+const NavLink = ({displayText, linkLocation, iconImage}) => {
   return <>
   <Link to={linkLocation}>
-    <div className='nav-link'>{displayText}</div>
+    <div className='nav-link'>
+      <img alt={displayText} src={iconImage} />
+      {displayText}
+    </div>
   </Link>
   </>
 }
