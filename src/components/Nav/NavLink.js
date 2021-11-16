aa9409f5da41212aad389b88c2209abc601e43a7
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-16 13:40:18 -0500

    Added docs and reformated component

--- src/components/Nav/NavLink.js
+++ src/components/Nav/NavLink.js
@@ -1,5 +1,17 @@
-const NavLink = ({text}) => {
-  return <div className='nav-link'>{text}</div>
+import { Link } from 'react-router-dom';
+
+/**
+ * Button that goes on the navbar
+ * @param  {String} displayText What is displayed on the button
+ * @param  {String} linkLocation The react-router-dom {Link} location
+ * @return {JSX}     JSX for the link
+ */
+const NavLink = ({displayText, linkLocation}) => {
+  return <>
+  <Link to={linkLocation}>
+    <div className='nav-link'>{displayText}</div>
+  </Link>
+  </>
 }
 
 export default NavLink;
