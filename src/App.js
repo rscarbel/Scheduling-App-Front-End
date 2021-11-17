f5b7689adf179a8effe6599f71ae5cd6d3f2f7d3
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-17 15:28:13 -0500

    moved routes to top-level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import BottomNavbar from './components/Nav/BottomNavbar';
 import TopNavbar from './components/Nav/TopNavbar';
+import ScheduleDay from "./pages/ScheduleDay";
+import Employees from "./pages/Employees";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import CreateAccount from "./pages/CreateAccount";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TopNavbar />
-      <BottomNavbar />
+      <Router>
+        <TopNavbar />
+        <div className='page-content'>
+          <Switch>
+            <Route path='/' exact component={ScheduleDay}/>
+            <Route path='/create-account' exact component={CreateAccount} />
+            <Route path='/schedule-day' exact component={ScheduleDay}/>
+            <Route path='/employees' exact component={Employees}/>
+            <Route path='/account' exact component={Account}/>
+            <Route path='/logout' exact component={Logout}/>
+            <Route path='/login' exact component={Login}/>
+          </Switch>
+        </div>
+        <BottomNavbar />
+      </Router>
     </div>
   );
 }
