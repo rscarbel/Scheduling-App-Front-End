79a3edab2667da438f69b35e3373321f9bf6722f
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-17 15:29:26 -0500

    took away routes

--- src/components/Nav/BottomNavbar.js
+++ src/components/Nav/BottomNavbar.js
@@ -8,22 +8,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const BottomNavbar = () => {
   return <>
-    <Router>
-      <div className='page-content'>
-    <Switch>
-        <Route path='/' exact component={ScheduleDay}/>
-        <Route path='/schedule-day' exact component={ScheduleDay}/>
-        <Route path='/employees' exact component={Employees}/>
-        <Route path='/account' exact component={Account}/>
-        <Route path='/logout' exact component={Logout}/>
-        <Route path='/login' exact component={Login}/>
-      </Switch>
-      </div>
       <nav className='bottom-nav'>
       <BottomNavLink displayText='Schedule' iconImage='assets/home_icon.png' linkLocation='/schedule-day' />
       <BottomNavLink displayText='Employees' iconImage='assets/employees_icon.png' linkLocation='/employees' />
       </nav>
-    </Router>
   </>
 };
 
